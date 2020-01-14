9d98faf2d2dab3bc2e54be10e9e6d2504ebd65ab
nick <nicholas.johnston@sheridancollege.ca>
2020-01-14 09:39:31 -0500

    Added solutions files

---
+++ code/simpleform.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+<?php
+	echo "You entered ".$_GET['textbox']." <br>";
+?>
+<form action="" method="get">
+	<input type="text" name="textbox"><br>
+	<input type="submit">
+</form>
+</body>
+</html>
